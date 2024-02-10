3cbe2c3cdbd1caffae7ab14199882f8cb5bedaa7
Carlos Julian De La O Lopez <cjdelaol@concredito.com.mx>
2024-02-10 01:39:57 -0600

    Update login form to include email and password fields, and add a toggle to switch between login and registration. (generated by blackbox.ai )

--- src/components/mainpage/LoginMenu.vue
+++ src/components/mainpage/LoginMenu.vue
@@ -11,21 +11,34 @@
                 <v-list>
                     
                     <v-list-item>
-                        <v-text-field density="compact" variant="outlined" label="Search templates" single-line hide-details
+                        <v-text-field density="compact" variant="outlined" label="Correo" single-line hide-details
                         ></v-text-field>
                     </v-list-item>
 
                     <v-list-item>
-                        <v-switch color="purple" label="Enable hints" hide-details></v-switch>
+                        <v-text-field density="compact" variant="outlined" label="Password" type="password" single-line hide-details
+                        ></v-text-field>
+                    </v-list-item>
+                    <v-list-item v-if="isNewRegister">
+                        <v-text-field density="compact" variant="outlined" label="Nombre" single-line hide-details
+                        ></v-text-field>
+                    </v-list-item>
+                    <v-list-item v-if="isNewRegister">
+                        <v-text-field density="compact" variant="outlined" label="Apellido Paterno"  single-line hide-details
+                        ></v-text-field>
+                    </v-list-item>
+                    <v-list-item v-if="isNewRegister">
+                        <v-text-field density="compact" variant="outlined" label="Apellido Materno" single-line hide-details
+                        ></v-text-field>
                     </v-list-item>
                     <v-list-item>
-                        <a>Aun no estas registrado?</a>
+                        <div class="text-caption cursor-pointer text-center text-blue" @click="toogleTypeLogin"> {{isNewRegister ? 'Ya tienes cuenta?' : 'Aun no te haz registrado?'}}</div>
                     </v-list-item>
                 </v-list>
 
                 <v-card-actions>
                     <v-btn class="mx-auto" color="primary" variant="text" @click="menu = false">
-                        Iniciar Sesion
+                        {{isNewRegister ? 'Registrar' : 'Iniciar Sesion'}}
                     </v-btn>
                 </v-card-actions>
             </v-card>
@@ -37,7 +50,15 @@ import { ref } from 'vue'
 export default {
     setup() {
         const menu = ref(false)
+        const isNewRegister = ref(false)
+
+        const toogleTypeLogin = () => {
+            isNewRegister.value = !isNewRegister.value;
+        }
+
         return {
+            toogleTypeLogin,
+            isNewRegister,
             menu
         }
     }
